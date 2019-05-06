757136b6a94c38a929100ad9aa198544a6fbc101
aagni <agn.madi@gmail.com>
2019-05-06 12:26:25 +0100

    add ui code to areaActivity
    add code to go from area activity to merchant activity (area part)

--- app/src/main/java/com/example/geopay/Area.java
+++ app/src/main/java/com/example/geopay/Area.java
@@ -2,13 +2,55 @@ package com.example.geopay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Area extends AppCompatActivity {
+
+    public static final String AREA = "";
+    Spinner spiner;
+    String selectedArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
+
+        //get username from main activity and set up the spinner
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.USERNAME);
+
+        if(message != "")
+        {
+            spiner = findViewById(R.id.spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.geo_mock_areas, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spiner.setAdapter(adapter);
+
+            spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    selectedArea = parent.getSelectedItem().toString();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // show some msg
+                }
+            });
+        }
+
+    }
+
+// Control events
+    public void goToMerchants(View view)
+    {
+        Intent intent = new Intent(this, Merchants.class);
+        intent.putExtra(AREA, selectedArea);
+        startActivity(intent);
     }
 }
