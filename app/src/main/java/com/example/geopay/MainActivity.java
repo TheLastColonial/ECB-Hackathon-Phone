c5579f1ab0c471e2ab1d11a1dc4cfe4f5600e8d5
aagni <agn.madi@gmail.com>
2019-05-06 15:04:51 +0100

    add notification test
    btn on subscribe success page to test notification - it will pop up notification that after clic will take user to form with pin that will initiate payment

--- app/src/main/java/com/example/geopay/MainActivity.java
+++ app/src/main/java/com/example/geopay/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.geopay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String USERNAME = "";
+    private static final String CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //register notification
+        this.createNotificationChannel();
     }
 
     //Controls events
@@ -28,6 +35,23 @@ public class MainActivity extends AppCompatActivity {
         {
             intent.putExtra(USERNAME, message);
             startActivity(intent);
+        }
+    }
+
+    //Notifications
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.geo_cha_name);
+            String description = getString(R.string.geo_cha_desc);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
