50c47d255b9675dcb32cfbca5f01f304d66d7c7f
Matesz Johanik <info.powrotdokorzeni@gmail.com>
2019-05-06 13:33:26 +0100

    added intentservice added geofecnefactoryfactory

--- app/src/main/java/com/example/geopay/GeofenceService.java
+++ app/src/main/java/com/example/geopay/GeofenceService.java
@@ -1,8 +1,19 @@
 package com.example.geopay;
 
 // GeofenceErrorMessages pakcages
+import android.app.IntentService;
+
+import android.content.Intent;
+
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
+
+// to get geofencingclient api
+import com.google.android.gms.location.GeofencingEvent;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
 
 class GeofenceModel
 {
@@ -20,6 +31,11 @@ interface IGeofenceService {
 
 public class GeofenceService implements IGeofenceService{
 
+    public void Initialize()
+    {
+
+    }
+
     @Override
     public void AddGeofence(int id, java.lang.String geomerReference, double latitude, double longitude, float radius) {
 
@@ -33,6 +49,7 @@ public class GeofenceService implements IGeofenceService{
 
 }
 
+// geofence api errors
 class GeofenceErrorMessages {
 
     private static java.lang.String geofence_not_available = "geofence not available";
