a99a7768955c2e5a3964152aecbbc3894a5faf97
aagni <agn.madi@gmail.com>
2019-05-06 13:48:44 +0100

    subscribe

--- app/src/main/java/com/example/geopay/Subscribe.java
+++ app/src/main/java/com/example/geopay/Subscribe.java
@@ -1,14 +1,28 @@
 package com.example.geopay;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Subscribe extends AppCompatActivity {
+
+    String sbsMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscribe);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Merchants.SELECTED_MERCH);
+
+        if(message != "")
+        {
+            TextView txt = (TextView)findViewById(R.id.txtYouSbus);
+            sbsMsg = getResources().getString(R.string.geo_succ) + " " + message;
+            txt.setText(sbsMsg);
+        }
     }
 }
