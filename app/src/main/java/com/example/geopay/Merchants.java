79b040c73a6013cf058804acbc860487a2f506d5
aagni <agn.madi@gmail.com>
2019-05-06 09:42:22 +0100

    add merchants

---
+++ app/src/main/java/com/example/geopay/Merchants.java
@@ -0,0 +1,14 @@
+package com.example.geopay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Merchants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchants);
+    }
+}
