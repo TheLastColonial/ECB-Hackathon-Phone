e5e21f649dec2b7c29118ed7028b7daed49d9c73
Matesz Johanik <info.powrotdokorzeni@gmail.com>
2019-05-06 12:39:18 +0100

    import google services apis

--- app/src/main/java/com/example/geopay/GeofenceService.java
+++ app/src/main/java/com/example/geopay/GeofenceService.java
@@ -1,5 +1,10 @@
 package com.example.geopay;
 
+// GeofenceErrorMessages pakcages
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.GeofenceStatusCodes;
+
+
 interface IGeofenceService {
     void AddGeofence(int id, double  latitude, double longitude, float radius);
 }
@@ -9,5 +14,37 @@ public class GeofenceService implements IGeofenceService{
     @Override
     public void AddGeofence(int id, double latitude, double longitude, float radius) {
 
+    }
+}
+
+class GeofenceErrorMessages {
+
+    private static java.lang.String geofence_not_available = "geofence not available";
+    private static java.lang.String geofence_too_many_geofences = "geofence too many geofences";
+    private static java.lang.String geofence_too_many_pending_intents = "geofence too many pending intensts";
+    private static java.lang.String unknown_geofence_error = "unknown geofence error";
+
+    private GeofenceErrorMessages() {}
+
+    public static String getErrorString(Exception e) {
+        if (e instanceof ApiException) {
+            return getErrorString(((ApiException) e).getStatusCode());
+        } else {
+            return "unknown exception";
+        }
+    }
+
+    public static String getErrorString(int errorCode) {
+
+        switch (errorCode) {
+            case GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE:
+                return GeofenceErrorMessages.geofence_not_available;
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES:
+                return GeofenceErrorMessages.geofence_too_many_geofences;
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                return GeofenceErrorMessages.geofence_too_many_pending_intents;
+            default:
+                return GeofenceErrorMessages.unknown_geofence_error;
+        }
     }
 }
