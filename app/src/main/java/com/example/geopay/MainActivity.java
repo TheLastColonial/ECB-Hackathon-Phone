19e7e404e3ba6750ed00b3c4c543f5112f945a70
aagni <agn.madi@gmail.com>
2019-05-06 11:08:20 +0100

    Main to Choose Area step
    add code to move from mainActivity to area Activity if user will add username

--- app/src/main/java/com/example/geopay/MainActivity.java
+++ app/src/main/java/com/example/geopay/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.geopay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USERNAME = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Controls events
+    public void goToAreas(View view)
+    {
+        Intent intent = new Intent(this, Area.class);
+        EditText username = (EditText) findViewById(R.id.txtUsername);
+        String message = username.getText().toString();
+
+        if(message != "")
+        {
+            intent.putExtra(USERNAME, message);
+            startActivity(intent);
+        }
     }
 }
